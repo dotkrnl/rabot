3fdfa553696c8d3c955b3dddcfd0f2b17a99cf8e
mato_no1 <mato_no1@yeah.net>
2015-11-13 02:44:41 +0000

    Update backend archive function, now every submission will produce an archive

--- backend/archive/managers.py
+++ backend/archive/managers.py
@@ -11,30 +11,77 @@ class ArchiveManger():
         for archive in all_archive:
             self.dao.delete_archive(archive)
 
-    def save(self, uid, sid, info):
+    def add_archive(self, uid, sid, src, result):
         if self.dao.user_and_stage_is_valid(uid, sid):
-            target = self.dao.get_archive_by_uid_and_sid(uid, sid)
-            if target:
-                self.dao.update_archive_info(target, info)
-                return 'Succeeded. Updated archive #' + str(target.arid)
+            all_archive = self.dao.get_all_archive()
+            if len(all_archive) > 0:
+                arid = all_archive[-1].arid + 1
             else:
-                all_archive = self.dao.get_all_archive()
-                if len(all_archive) > 0:
-                    arid = all_archive[-1].arid + 1
-                else:
-                    arid = 1
+                arid = 1
 
-                self.dao.create_archive(arid, uid, sid, info)
-                return 'Succeeded. New archive #' + str(arid)
+            self.dao.create_archive(arid, uid, sid, src, result)
+            return 'Succeeded. New archive #' + str(arid)
         else:
             return 'Uid or sid is invalid.'
 
-    def load(self, uid, sid):
-        if self.dao.user_and_stage_is_valid(uid, sid):
-            target = self.dao.get_archive_by_uid_and_sid(uid, sid)
-            if target:
-                return target
+    def update_archive(self, arid, new_uid, new_sid, new_src, new_result):
+        cur_archive = self.dao.get_archive_by_arid(arid)
+        if cur_archive:
+            if new_uid > 0:
+                self.dao.update_archive_uid(cur_archive, new_uid)
+            if new_sid > 0:
+                self.dao.update_archive_sid(cur_archive, new_sid)
+            if len(new_src) > 0:
+                self.dao.update_archive_src(cur_archive, new_src)
+            if len(new_result) > 0:
+                self.dao.update_archive_result(cur_archive, new_result)
+            return 'Succeeded.'
+        else:
+            return 'Archive #%s does not exist' % (str(arid))
+
+    def delete_archive(self, arid):
+        cur_archive = self.dao.get_archive_by_arid(arid)
+        if cur_archive:
+            self.dao.delete_archive(cur_archive)
+            return 'Succeeded.'
+        else:
+            return 'Succeeded, nothing is done.'
+
+    def search_archive(self, uid, sid, arid=0):
+        if arid > 0:
+            cur_archive = self.dao.get_archive_by_arid(arid)
+            if cur_archive and cur_archive.uid == uid and cur_archive.sid == sid:
+                return [cur_archive]
             else:
-                return None
+                return []
+
+        else:
+            targets = self.dao.get_all_archive_by_uid_and_sid(uid, sid)
+            return targets
+
+    def load_code(self, uid, sid):
+        if self.dao.user_and_stage_is_valid(uid, sid):
+            targets = self.dao.get_all_archive_by_uid_and_sid(uid, sid)
+            if len(targets) > 0:
+                return targets[-1].src
+            else:
+                return ''
         else:
             return None
+
+    def get_result(self, uid, sid):
+        if self.dao.user_and_stage_is_valid(uid, sid):
+            targets = self.dao.get_all_archive_by_uid_and_sid(uid, sid)
+            if len(targets) > 0:
+                return targets[-1].result
+            else:
+                return ''
+        else:
+            return None
+
+    def get_stage_record_count(self, sid):
+        if self.dao.user_and_stage_is_valid(1, sid):
+            targets = self.dao.get_all_archive_by_uid_and_sid(0, sid)
+            return len(targets)
+        else:
+            return 0
--- backend/archive/models.py
+++ backend/archive/models.py
@@ -9,14 +9,17 @@ class Archive(models.Model):
     arid = models.IntegerField(default=0, unique=True)
     uid = models.IntegerField(default=0)
     sid = models.IntegerField(default=0)
-    info = models.TextField(null=True)
+    submit_time = models.DateTimeField(auto_now_add=True)
+    src = models.TextField(null=True)
+    result = models.TextField(null=True)
 
     def __unicode__(self):
-        return 'User #' + str(self.uid) + ', Stage #' + str(self.sid) + ', content = ' + self.info
+        return 'User: #%s;\nStage #%s;\nCode: \n[CODE_BEGIN]\n%s[CODE_END]\n;\nResult: %s;\n' % (str(self.uid), str(self.sid), self.src, self.result)
 
     class Meta:
         db_table = 'archive'
         ordering = ['arid']
+        get_latest_by = ['submit_time']
 
 
 class ArchiveDao():
@@ -38,7 +41,19 @@ class ArchiveDao():
             return False
 
     def get_all_archive(self):
-        return list(Archive.objects.all())
+        return list(Archive.objects.all().order_by('submit_time'))
+
+    def get_all_archive_by_uid_and_sid(self, uid=0, sid=0):
+        if uid > 0 and sid > 0:
+            targets = Archive.objects.filter(uid=uid, sid=sid).order_by('submit_time')
+        elif uid > 0:
+            targets = Archive.objects.filter(uid=uid).order_by('submit_time')
+        elif sid > 0:
+            targets = Archive.objects.filter(sid=sid).order_by('submit_time')
+        else:
+            targets = Archive.objects.all().order_by('submit_time')
+
+        return list(targets)
 
     def get_archive_by_arid(self, arid):
         try:
@@ -48,20 +63,24 @@ class ArchiveDao():
         else:
             return target
 
-    def get_archive_by_uid_and_sid(self, uid, sid):
-        try:
-            target = Archive.objects.get(uid=uid, sid=sid)
-        except Archive.DoesNotExist:
-            return None
-        else:
-            return target
+    def create_archive(self, arid, uid, sid, src, result):
+        Archive.objects.create(arid=arid, uid=uid, sid=sid, src=src, result=result)
 
-    def create_archive(self, arid, uid, sid, info):
-        Archive.objects.create(arid=arid, uid=uid, sid=sid, info=info)
+    def update_archive_uid(self, cur_archive, uid):
+        cur_archive.uid = uid
+        cur_archive.save(update_fields=['uid'])
 
-    def update_archive_info(self, cur_archive, info):
-        cur_archive.info = info
-        cur_archive.save(update_fields=['info'])
+    def update_archive_sid(self, cur_archive, sid):
+        cur_archive.sid = sid
+        cur_archive.save(update_fields=['sid'])
+
+    def update_archive_src(self, cur_archive, src):
+        cur_archive.src = src
+        cur_archive.save(update_fields=['src'])
+
+    def update_archive_result(self, cur_archive, result):
+        cur_archive.result = result
+        cur_archive.save(update_fields=['result'])
 
     def delete_archive(self, cur_archive):
         cur_archive.delete()
